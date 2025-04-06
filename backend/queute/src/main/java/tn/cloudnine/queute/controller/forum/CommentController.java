1386c991cb6968e84fb8abf330b23f313ba69c06
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 05:59:13 +0100

    FEAT: Comment crud

---
+++ backend/queute/src/main/java/tn/cloudnine/queute/controller/forum/CommentController.java
@@ -0,0 +1,39 @@
+package tn.cloudnine.queute.controller.forum;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.cloudnine.queute.model.forum.Comment;
+import tn.cloudnine.queute.serviceImpl.forum.CommentService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("create-comment")
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.create(comment));
+    }
+    @PutMapping("update-comment")
+    public ResponseEntity<Comment> updateComment(@RequestBody Comment comment) {
+        return ResponseEntity.ok().body(commentService.update(comment));
+    }
+    @DeleteMapping("delete-comment/{id}")
+    public ResponseEntity<Comment> deleteComment(@PathVariable long id) {
+        //TODO: check for comment existence
+        commentService.delete(id);
+        var comment = commentService.findById(id);
+        return ResponseEntity.ok().body(comment);
+    }
+    @GetMapping("get-comments/{id}")
+    public ResponseEntity<Comment> getComment(@PathVariable Long id) {
+        return ResponseEntity.ok().body(commentService.findById(id));
+    }
+    @GetMapping("get-comments")
+    public ResponseEntity<List<Comment>> getComments() {
+        return ResponseEntity.ok().body(commentService.findAll());
+    }
+}
