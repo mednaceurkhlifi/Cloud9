47d22d0c3eb9318a8b7c07ddb420f46cd739656d
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 17:02:19 +0100

    FEAT: vote service

---
+++ backend/queute/src/main/java/tn/cloudnine/queute/serviceImpl/forum/VoteService.java
@@ -0,0 +1,58 @@
+package tn.cloudnine.queute.serviceImpl.forum;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.cloudnine.queute.model.forum.Vote;
+import tn.cloudnine.queute.repository.forum.VoteRepository;
+import tn.cloudnine.queute.service.forum.IVoteService;
+import tn.cloudnine.queute.service.user.IUserService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VoteService implements IVoteService {
+
+    private final VoteRepository voteRepository;
+    private final IUserService userService;
+    @Override
+    public Vote create(Vote vote) {
+        //TODO: add vote to the users list
+        var user = userService.findById(vote.getUser().getUser_id());
+        vote.setUser(user);
+        return voteRepository.save(vote);
+    }
+
+    @Override
+    public Vote update(Vote vote) {
+        //TODO: update vote to the users list
+        Vote oldVote = findById(vote.getId());
+        oldVote.setVoteType(vote.getVoteType());
+
+        return voteRepository.save(oldVote);
+    }
+
+    @Override
+    public Vote delete(long id) {
+        //TODO: remove vote from the users list
+        try{
+            Vote vote = findById(id);
+            voteRepository.deleteById(id);
+            return vote;
+        }catch (EntityNotFoundException e){
+            return null;
+        }
+    }
+
+    @Override
+    public Vote findById(Long id) {
+        Vote vote = voteRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Vote not found with id :"+id));
+        return vote;
+    }
+
+    @Override
+    public List<Vote> findAll() {
+        return voteRepository.findAll();
+    }
+}
