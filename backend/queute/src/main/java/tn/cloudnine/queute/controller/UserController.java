2fa3c749c1590a47ee78ee33d67a66a3fcfa3f47
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 17:02:05 +0100

    FEAT: user controller

---
+++ backend/queute/src/main/java/tn/cloudnine/queute/controller/UserController.java
@@ -0,0 +1,24 @@
+package tn.cloudnine.queute.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tn.cloudnine.queute.model.forum.Vote;
+import tn.cloudnine.queute.model.user.User;
+import tn.cloudnine.queute.service.forum.IVoteService;
+import tn.cloudnine.queute.service.user.IUserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class UserController {
+
+    public final IUserService userService;
+    @PostMapping("create-user")
+    public ResponseEntity<User> createVote(@RequestBody User user) {
+        return ResponseEntity.ok().body(userService.create(user));
+    }
+}
