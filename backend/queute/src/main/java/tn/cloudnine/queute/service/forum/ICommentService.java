077c840f75e6a0b2d1e838452c23e07787cafaaf
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 05:58:41 +0100

    FEAT: added methods to interface

--- backend/queute/src/main/java/tn/cloudnine/queute/service/forum/ICommentService.java
+++ backend/queute/src/main/java/tn/cloudnine/queute/service/forum/ICommentService.java
@@ -1,4 +1,15 @@
 package tn.cloudnine.queute.service.forum;
 
+import tn.cloudnine.queute.model.forum.Comment;
+import tn.cloudnine.queute.model.forum.Post;
+
+import java.util.List;
+
 public interface ICommentService {
+
+    public Comment create(Comment comment);
+    public Comment update(Comment comment);
+    public Comment delete(long id);
+    public Comment findById(Long id);
+    public List<Comment> findAll();
 }
