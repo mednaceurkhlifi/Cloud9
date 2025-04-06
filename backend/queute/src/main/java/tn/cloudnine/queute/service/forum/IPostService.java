4b1db086ec677dd7d709c7094adf598b2419e59b
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 04:38:50 +0100

    FEAT: post service

--- backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IPostService.java
+++ backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IPostService.java
@@ -1,4 +1,13 @@
 package tn.cloudnine.queute.service.forum;
 
+import tn.cloudnine.queute.model.forum.Post;
+
+import java.util.List;
+
 public interface IPostService {
+    public Post create(Post post);
+    public Post update(Post post);
+    public void delete(Post post);
+    public Post findById(Long id);
+    public List<Post> findAll();
 }
