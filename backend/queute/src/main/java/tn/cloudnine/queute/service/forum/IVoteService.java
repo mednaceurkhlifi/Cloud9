4f3a2dde69b581d8c71ce949f53e05ec69eb7051
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-04-06 17:01:39 +0100

    FEAT: service interfaces

--- backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IVoteService.java
+++ backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IVoteService.java
@@ -1,4 +1,15 @@
 package tn.cloudnine.queute.service.forum;
 
+import tn.cloudnine.queute.model.forum.Comment;
+import tn.cloudnine.queute.model.forum.Vote;
+
+import java.util.List;
+
 public interface IVoteService {
+
+    public Vote create(Vote vote);
+    public Vote update(Vote vote);
+    public Vote delete(long id);
+    public Vote findById(Long id);
+    public List<Vote> findAll();
 }
