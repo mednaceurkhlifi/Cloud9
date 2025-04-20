8918f816f27450dcee68e5dedfdfcb8b772be8f6
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-04-21 00:23:49 +0100

    FEAT: toxicity response

---
+++ backend/queute/src/main/java/tn/cloudnine/queute/dto/forum/ToxicityReponse.java
@@ -0,0 +1,10 @@
+package tn.cloudnine.queute.dto.forum;
+
+public class ToxicityReponse {
+    private double identity_attack;
+    private double insult;
+    private double obscene;
+    private double severe_toxicity;
+    private double threat;
+    private double toxicity;
+}
