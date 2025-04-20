0683d4b23aa384ced00c37699cf38e0942956064
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-04-21 00:11:45 +0100

    DEP: flask api service

--- backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IFlaskService.java
+++ backend/queute/src/main/java/tn/cloudnine/queute/service/forum/IFlaskService.java
@@ -1,12 +1,11 @@
 package tn.cloudnine.queute.service.forum;
 
-import tn.cloudnine.queute.dto.forum.PostDTO;
 import tn.cloudnine.queute.enums.forum.SentimentType;
 import tn.cloudnine.queute.model.forum.Post;
 import tn.cloudnine.queute.model.forum.Votable;
 
-public interface FlaskService {
-    public String Summurizer(Post post);
+public interface IFlaskService {
+    public String Summarizer(Post post);
     public SentimentType sentimentAnalysis(Votable votable);
     public boolean isToxic(Votable votable);
 }
---
+++ backend/queute/src/main/java/tn/cloudnine/queute/serviceImpl/forum/FlaskService.java
@@ -0,0 +1,49 @@
+package tn.cloudnine.queute.serviceImpl.forum;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import tn.cloudnine.queute.dto.forum.SentimentResponse;
+import tn.cloudnine.queute.enums.forum.SentimentType;
+import tn.cloudnine.queute.model.forum.Post;
+import tn.cloudnine.queute.model.forum.Votable;
+import tn.cloudnine.queute.service.forum.IFlaskService;
+
+import java.util.Map;
+
+@Service
+public class FlaskService implements IFlaskService {
+    private final WebClient webclient = WebClient.create("http://localhost:5000");
+    @Override
+    public String Summarizer(Post post) {
+        return "";
+    }
+
+    @Override
+    public SentimentType sentimentAnalysis(Votable votable) {
+        var ret =webclient.post()
+                .uri("/sentiment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("text",votable.getContent()))
+                .retrieve()
+                .bodyToMono(SentimentResponse.class)
+                .block();
+        if(ret != null){
+            if(ret.getLabel().equals("LABEL_0")){
+                return SentimentType.NEGATIVE;
+            }
+            else if(ret.getLabel().equals("LABEL_1")){
+                return SentimentType.NEUTRAL;
+            }
+            else if(ret.getLabel().equals("LABEL_2")){
+                return SentimentType.POSITIVE;
+            }
+        }
+        return SentimentType.NEUTRAL;
+    }
+
+    @Override
+    public boolean isToxic(Votable votable) {
+        return false;
+    }
+}
